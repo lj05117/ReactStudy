573f5f487b247a050216957a5b74076034e3ae63
lj05117 <lj05117@naver.com>
2022-09-18 02:49:14 +0900

    weather and complete

--- 노마드/바닐라JS로크롬앱만들기/momentum/js/weather.js
+++ 노마드/바닐라JS로크롬앱만들기/momentum/js/weather.js
@@ -4,7 +4,12 @@ function onGeoOk(position) {
     const lng = position.coords.longitude;
     console.log("You live in", lat, lng);
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
-    console.log(url);
+    fetch(url).then((response) => response.json().then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }))
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.");
