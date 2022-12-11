793e51beeb95eaf684fa820b9cd6b1493ed7ea53
lj05117 <lj05117@naver.com>
2022-12-11 21:41:15 +0900

    Add: Add new file for making page form

--- MyPortfolio/my_protfolio/src/index.js
+++ MyPortfolio/my_protfolio/src/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { InView, useInView } from "react-intersection-observer";
-// import Introduce from "./elements/Introduce";
+// import { InView, useInView } from "react-intersection-observer";
+import Introduce from "./elements/Introduce";
+import Contact from "./elements/Contact";
+import Skill from "./elements/Skill";
+import Project from "./elements/Project";
 
 const App = () => {
-  const [ref, inView]=useInView()
-  return (
-    <div ref={ref}>
-      Element {InView.toString()}
-    </div>
-  )
+  // const [ref, inView]=useInView()
+  // return (
+    // <div ref={ref}>
+      // Element {InView.toString()}
+    // </div>
+  // )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Introduce />
+    <Skill />
+    <Project/>
+    <Contact/>
   </React.StrictMode>
 );
 
