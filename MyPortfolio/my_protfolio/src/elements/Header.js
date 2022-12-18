e2cbd772bbb4eef567eb072626c284a103f64562
lj05117 <lj05117@naver.com>
2022-12-19 00:20:50 +0900

    feat: add router

--- MyPortfolio/my_protfolio/src/elements/Header.js
+++ MyPortfolio/my_protfolio/src/elements/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import style from "./css/Header.module.css";
+import { Link } from "react-router-dom";
 function Header() {
     return (
         <nav className={`${style.nav}`}>
             <ul className={`${style.headerUl}`}>
-                <li className={`${style.headerLi}`}>Introduce</li>
-                <li className={`${style.headerLi}`}>Skill</li>
-                <li className={`${style.headerLi}`}>Project</li>
-                <li className={`${style.headerLi}`}>Contact</li>
+                <Link to="/introduce"><li className={`${style.headerLi}`}>Introduce</li></Link>
+                <Link to="/skill"><li className={`${style.headerLi}`}>Skill</li></Link>
+                <Link to="/project"><li className={`${style.headerLi}`}>Project</li></Link>
+                <Link to="/contact"><li className={`${style.headerLi}`}>Contact</li></Link>
             </ul>
         </nav>
     );
--- MyPortfolio/my_protfolio/src/index.js
+++ MyPortfolio/my_protfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { InView, useInView } from "react-intersection-observer";
 import Introduce from "./elements/Introduce";
 import Contact from "./elements/Contact";
@@ -7,23 +8,27 @@ import Skill from "./elements/Skill";
 import Project from "./elements/Project";
 import Header from "./elements/Header";
 
-const App = () => {
-  // const [ref, inView]=useInView()
-  // return (
-    // <div ref={ref}>
-      // Element {InView.toString()}
-    // </div>
-  // )
-}
+// const App = () => {
+//   // const [ref, inView]=useInView()
+//   // return (
+//     // <div ref={ref}>
+//       // Element {InView.toString()}
+//     // </div>
+//   // )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <Introduce />
-    <Skill />
-    <Project/>
-    <Contact/>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/introduce" element={<Introduce />}></Route>   
+        <Route path="/skill" element={<Skill />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
