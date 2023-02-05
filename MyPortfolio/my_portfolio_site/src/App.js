663557741b93099cacb7d94b9b5c1f1d98659472
lj05117 <lj05117@naver.com>
2023-02-05 13:43:15 +0900

    fix : 오타 수정

--- MyPortfolio/my_portfolio_site/src/App.js
+++ MyPortfolio/my_portfolio_site/src/App.js
@@ -1,44 +1,39 @@
-import React,{useState/*, useRef, useMemo, useEffect*/} from "react";
+import React,{useState, useRef, useMemo, useEffect} from "react";
 
 
 function App(){
-    // const targetRef = useRef(null);
+    const targetRef = useRef(null);
     const [isVisible, setIsVisible] = useState(false);
 
-    // const callbackFunction = entries =>{
-    //     const [entry]=entries; //const entry = entries[0];
-    //     setIsVisible(entry.isIntersecting);
-    // }
+    const callbackFunction = entries =>{
+        const [entry]=entries; //const entry = entries[0];
+        setIsVisible(entry.isIntersecting);
+    }
 
-    // const options=useMemo(()=>{
-    //     return {
-    //         root:null,
-    //         rootMargin:'0px',
-    //         threshold:0.3
-    //     } 
-    // },[]);
-    // {
-    //     root:null,
-    //     rootMargin:'0px',
-    //     threshold:0.3
-    // }
-    
-    // useEffect(()=>{
-    //     const observer = new IntersectionObserver(callbackFunction, options);
-    //     const currentTarget = targetRef.current;
-    //     if(currentTarget) observer.observer(currentTarget);
+    const options=useMemo(()=>{
+        return {
+            root:null,
+            rootMargin:'0px',
+            threshold:0.3
+        } 
+    },[]);
 
-    //     return()=>{
-    //         if(currentTarget) observer.unobserve(currentTarget);
-    //     }
-    // }, [targetRef, options]);
+    useEffect(()=>{
+        const observer = new IntersectionObserver(callbackFunction, options);
+        const currentTarget = targetRef.current;
+        if(currentTarget) observer.observe(currentTarget);
+
+        return()=>{
+            if(currentTarget) observer.unobserve(currentTarget);
+        }
+    }, [targetRef, options]);
 
 
     return (
         <>
         <h1 className="header"><p>{!isVisible?'not in viewport':'in viewport'}</p></h1>
         <div className="gap"></div>
-        <img alt="temp" src="Img/port.PNG" /*ref={targetRef}*/></img>
+        <img alt="temp" src="Img/port.PNG" ref={targetRef}></img>
         </>
     );
 }
